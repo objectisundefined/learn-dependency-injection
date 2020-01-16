74fa865fc5d0b1acb805f4ef6dcd5e778bac3dcb
objectisundefined <2535612040@qq.com>
2020-01-16 17:56:49 +0800

    feat(di): multi injection

---
+++ __tests__/multi.test.ts
@@ -0,0 +1,39 @@
+import { injectable, multiInject, Container } from 'inversify';
+import { Katana, Shuriken } from '../src/entities';
+import { Weapon, ThrowableWeapon } from '../src/interfaces';
+
+@injectable()
+class Ninja implements Ninja {
+  public katana: Weapon;
+  public shuriken: ThrowableWeapon;
+
+  public constructor(
+    @multiInject('Weapon') weapons: (Weapon | ThrowableWeapon)[]
+  ) {
+    this.katana = weapons[0] as Weapon;
+    this.shuriken = weapons[1] as ThrowableWeapon;
+  }
+
+  fight() {
+    return this.katana.hit();
+  }
+
+  sneak() {
+    return this.shuriken.throw();
+  }
+}
+
+const container = new Container();
+
+container.bind<Ninja>('Ninja').to(Ninja);
+container.bind<Weapon>('Weapon').to(Katana);
+container.bind<ThrowableWeapon>('Weapon').to(Shuriken);
+
+describe('multi injection', () => {
+  it('works', () => {
+    const ninja = container.get<Ninja>('Ninja');
+
+    expect(ninja.fight()).toEqual('cut!'); // true
+    expect(ninja.sneak()).toEqual('hit!'); // true
+  });
+});
