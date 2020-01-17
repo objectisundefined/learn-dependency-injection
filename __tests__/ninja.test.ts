9d6a8094b96cebc4cc5b8a0f220bc59b060f13c3
objectisundefined <2535612040@qq.com>
2020-01-17 10:18:10 +0800

    feat(di): integration test with dist files

--- __tests__/ninja.test.ts
+++ __tests__/ninja.test.ts
@@ -1,5 +1,8 @@
 import { ninja } from '../src';
 
+// test for integration
+// import { ninja } from '../dist/index'
+
 describe('ninja', () => {
   it('works', () => {
     expect(ninja.fight()).toEqual('cut!');
